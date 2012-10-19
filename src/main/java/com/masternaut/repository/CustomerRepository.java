d489371aa37619aec74cac0059213a43f2aba4c8
Adam Pridmore <adam_pridmore@hotmail.com>
2012-10-19 15:55:20 +0100

    added collection tidy up. added count method.

--- src/main/java/com/masternaut/repository/CustomerRepository.java
+++ src/main/java/com/masternaut/repository/CustomerRepository.java
@@ -1,13 +1,26 @@
 package com.masternaut.repository;
 
 import com.masternaut.domain.Customer;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 
 public class CustomerRepository {
+    private MongoTemplate mongoTemplate;
+
+    public CustomerRepository(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
     public void save(Customer customer) {
+        mongoTemplate.save(customer);
 
     }
 
     public Customer findById(String id) {
-        return new Customer();
+        return mongoTemplate.findById(id, Customer.class);
+    }
+
+    public long count() {
+        return mongoTemplate.count(new Query(),Customer.class);
     }
 }
--- src/test/java/com/masternaut/repository/CustomerRepositoryTest.java
+++ src/test/java/com/masternaut/repository/CustomerRepositoryTest.java
@@ -1,14 +1,41 @@
 package com.masternaut.repository;
 
 import com.masternaut.domain.Customer;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring/UnitTest-context.xml")
 public class CustomerRepositoryTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Before
+    public void before(){
+        mongoTemplate.dropCollection(Customer.class);
+    }
+
     @Test
-    public void saveAndLoad(){
-        CustomerRepository customerRepository = new CustomerRepository();
+    public void smokeTest() {
+        assertNotNull(context);
+    }
+
+    @Test
+    public void saveAndLoad() {
+        CustomerRepository customerRepository = new CustomerRepository(mongoTemplate);
 
         Customer customer = new Customer();
         customer.setName("MyCustomerName");
@@ -19,6 +46,18 @@ public class CustomerRepositoryTest {
 
         assertEquals(customer.getId(), loadedCustomer.getId());
         assertEquals("MyCustomerName", loadedCustomer.getName());
+    }
+
+    @Test
+    public void countRecords() {
+        CustomerRepository customerRepository = new CustomerRepository(mongoTemplate);
+
+        Customer customer = new Customer();
+        customer.setName("MyCustomerName");
+
+        customerRepository.save(customer);
+
+        assertEquals(1, customerRepository.count());
     }
 }
 
