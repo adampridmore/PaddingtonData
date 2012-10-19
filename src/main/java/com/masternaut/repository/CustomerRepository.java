e009c7b8e6d7f95edac134651324c569b8c9b5da
Adam Pridmore <adam_pridmore@hotmail.com>
2012-10-19 16:42:35 +0100

    - Doesn't use Spring to construct repository, and uses reflection.

--- src/main/java/com/masternaut/repository/CustomerRepository.java
+++ src/main/java/com/masternaut/repository/CustomerRepository.java
@@ -1,23 +1,18 @@
 package com.masternaut.repository;
 
 import com.masternaut.domain.Customer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Component;
 
-@Component
 public class CustomerRepository {
     private MongoTemplate mongoTemplate;
 
-    @Autowired
     public CustomerRepository(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
     }
 
     public void save(Customer customer) {
         mongoTemplate.save(customer);
-
     }
 
     public Customer findById(String id) {
--- src/main/java/com/masternaut/repository/RepositoryFactory.java
+++ src/main/java/com/masternaut/repository/RepositoryFactory.java
@@ -1,19 +1,27 @@
 package com.masternaut.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Constructor;
 
 @Component
 public class RepositoryFactory {
-    private ApplicationContext applicationContext;
+    private MongoTemplate mongoTemplate;
 
     @Autowired
-    public RepositoryFactory(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
+    public RepositoryFactory(MongoTemplate mongoTemplate) {
+
+        this.mongoTemplate = mongoTemplate;
     }
 
     public <T> T createRepository(Class<T> clazz) {
-        return applicationContext.getBean(clazz);
+        try {
+            Constructor<T> constructor = clazz.getConstructor(MongoTemplate.class);
+            return constructor.newInstance(mongoTemplate);
+        } catch(Throwable t) {
+            throw new RuntimeException(t);
+        }
     }
 }
