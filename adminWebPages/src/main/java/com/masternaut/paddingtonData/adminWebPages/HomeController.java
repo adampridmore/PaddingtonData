b3e08cd64674dbabcf8209b3e2e152e573802186
Adam Pridmore <adam_pridmore@hotmail.com>
2012-11-12 16:01:13 +0000

    MVC home page now show a list of customers.

--- adminWebPages/src/main/java/com/masternaut/paddingtonData/adminWebPages/HomeController.java
+++ adminWebPages/src/main/java/com/masternaut/paddingtonData/adminWebPages/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -20,10 +21,12 @@ public class HomeController {
     }
 
     @RequestMapping({"/", "/home"})
-    public String showHomePage(){
+    public String showHomePage(Map<String, Object> model){
         CustomerRepository repository = repositoryFactory.createRepository(CustomerRepository.class);
 
         List<Customer> customerList = repository.findAll();
+
+        model.put("customers", customerList);
 
         return "home";
     }
--- adminWebPages/src/main/webapp/WEB-INF/views/home.jsp
+++ adminWebPages/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,1 +1,15 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
 <strong>Hello World</strong>
+
+<ul>
+
+</ul>
+
+<c:forEach var="customer" items="${customers}">
+    <li>${customer.name}</li>
+</c:forEach>
+
+
